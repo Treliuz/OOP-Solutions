3eb582b053d14fdf87bd0f3f9080fdfa7f95f4a4
Treliuz <jmukuna15@gmail.com>
2025-04-03 14:48:23 -0400

    pre test customer order

---
+++ Assignments/Asgn-04/src/CustomerOrder.cpp
@@ -0,0 +1,104 @@
+#include "CustomerOrder.h"
+#include "Utilities.h"
+#include <algorithm>
+#include <iostream>
+#include <iomanip>
+
+namespace seneca {
+    size_t CustomerOrder::m_widthField = 0;
+    CustomerOrder::CustomerOrder() : m_name(""), m_product(""), m_cntItem(0), m_lstItem(nullptr)  {}
+
+    CustomerOrder::CustomerOrder(const std::string& str) {
+        Utilities util;
+        size_t next_pos = 0;
+        bool more = true;
+
+        m_name = util.extractToken(str, next_pos, more);
+        m_product = util.extractToken(str, next_pos, more);
+        size_t temp = next_pos;
+        while (more) {
+            util.extractToken(str, temp, more);
+            m_cntItem++;
+        }
+
+        m_lstItem = new Item*[m_cntItem];
+        for (size_t i = 0; i < m_cntItem; ++i) {
+            std::string item = util.extractToken(str, next_pos, more);
+            m_lstItem[i] = new Item(item);
+        }
+
+        if (m_widthField < util.getFieldWidth()) {
+            m_widthField = util.getFieldWidth();
+        }
+    }
+
+    CustomerOrder::CustomerOrder(CustomerOrder&& other) noexcept {
+        *this = std::move(other);
+    }
+
+    CustomerOrder& CustomerOrder::operator=(CustomerOrder&& other) noexcept {
+        if (this != &other) {
+            for (size_t i = 0; i < m_cntItem; i++) {
+                delete m_lstItem[i];
+            }
+            delete[] m_lstItem;
+
+            m_name = std::move(other.m_name);
+            m_product = std::move(other.m_product);
+            m_cntItem = other.m_cntItem;
+            m_lstItem = other.m_lstItem;
+        }
+        return *this;
+    }
+
+    CustomerOrder::~CustomerOrder() {
+        for (size_t i = 0; i < m_cntItem; i++) {
+            delete m_lstItem[i];
+        }
+        delete[] m_lstItem;
+    }
+
+    bool CustomerOrder::isOrderFilled() const {
+        return std::all_of(m_lstItem, m_lstItem + m_cntItem, [](const Item* item) {
+            return item->m_isFilled;
+        });
+    }
+
+    bool CustomerOrder::isItemFilled(const std::string& itemName) const {
+        bool found = false;
+        for (size_t i = 0; i < m_cntItem; i++) {
+            if (m_lstItem[i]->m_itemName == itemName) {
+                found = true;
+                if (!m_lstItem[i]->m_isFilled) {
+                    return false;
+                }
+            }
+        }
+        return !found || found;
+    }
+
+    void CustomerOrder::fillItem(Station& station, std::ostream& os) {
+        for (size_t i = 0; i < m_cntItem; i++) {
+            if (m_lstItem[i]->m_itemName == station.getItemName()) {
+                if (station.getQuantity() > 0 && !m_lstItem[i]->m_isFilled) {
+                    m_lstItem[i]->m_isFilled = true;
+                    m_lstItem[i]->m_serialNumber = station.getNextSerialNumber();
+                    station.updateQuantity();
+                    os << "Filled " << m_name << ", " << m_product << " [" << m_lstItem[i]->m_itemName << "]" << std::endl;
+                } else {
+                    os << "Unable to fill " << m_name << ", " << m_product << " [" << m_lstItem[i]->m_itemName << "]" << std::endl;
+                }
+                return;
+            }
+        }
+    }
+
+    void CustomerOrder::display(std::ostream& os) const {
+        os << m_name << " - " << m_product << std::endl;
+        for (size_t i = 0; i < m_cntItem; i++) {
+            os << "[" << std::setw(6) << std::setfill('0') << m_lstItem[i]->m_serialNumber << "] ";
+            os << std::setw(m_widthField) << std::left << std::setfill(' ') << m_lstItem[i]->m_itemName << " - ";
+            os << (m_lstItem[i]->m_isFilled ? "FILLED" : "TO BE FILLED") << std::endl;
+        }
+    }
+}
---
+++ Assignments/Asgn-04/src/CustomerOrder.h
@@ -0,0 +1,42 @@
+/********************************************************************************
+* OOP345 – Assignment 04
+*
+* I declare that this assignment is my own work in accordance with Seneca's
+* Academic Integrity Policy:
+*
+* Name: Joseph Mwamba-Mukuna Student ID: 163997216 Date: 03/04/2025  
+********************************************************************************/
+#ifndef SENECA_CUSTOMERORDER_H
+#define SENECA_CUSTOMERORDER_H
+#include "Station.h"
+#include <string>
+#include <stdexcept>
+namespace seneca {
+    class CustomerOrder {
+        struct Item {
+            std::string m_itemName;
+            size_t m_serialNumber{0};
+            bool m_isFilled{false};
+            Item(const std::string& src) : m_itemName(src) {};
+        };
+
+        std::string m_name;
+        std::string m_product;
+        size_t m_cntItem;
+        Item** m_lstItem;
+        static size_t m_widthField;
+    public:
+        CustomerOrder();
+        CustomerOrder(const std::string& str);
+        CustomerOrder(const CustomerOrder&) { throw std::runtime_error("ERROR: Cannot make copies."); }
+        CustomerOrder& operator=(const CustomerOrder&) = delete;
+        CustomerOrder(CustomerOrder&&) noexcept;
+        CustomerOrder& operator=(CustomerOrder&&) noexcept;
+        ~CustomerOrder();
+        bool isOrderFilled() const;
+        bool isItemFilled(const std::string& itemName) const;
+        void fillItem(Station& station, std::ostream& os);
+        void display(std::ostream& os) const;
+    };
+}
+#endif
